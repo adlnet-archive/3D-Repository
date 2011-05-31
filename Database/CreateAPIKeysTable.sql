da8c4d6aed01e3c9424c70d2aaa5582e32fcd89f
Rob Chadwick <robert.chadwick.ctr@adlnet.gov>
2011-05-31 10:28:37 +0000

    Added script to create apikeys table

---
+++ Database/CreateAPIKeysTable.sql
@@ -0,0 +1,128 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.5.10
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema apikeys
+--
+
+CREATE DATABASE IF NOT EXISTS apikeys;
+USE apikeys;
+
+--
+-- Definition of table `apikeys`
+--
+
+DROP TABLE IF EXISTS `apikeys`;
+CREATE TABLE `apikeys` (
+  `Email` varchar(255) NOT NULL,
+  `KeyText` varchar(45) NOT NULL,
+  `UsageText` varchar(1000) NOT NULL,
+  `State` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`KeyText`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `apikeys`
+--
+
+/*!40000 ALTER TABLE `apikeys` DISABLE KEYS */;
+/*!40000 ALTER TABLE `apikeys` ENABLE KEYS */;
+
+
+--
+-- Definition of procedure `DeleteKey`
+--
+
+DROP PROCEDURE IF EXISTS `DeleteKey`;
+
+DELIMITER $$
+
+/*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteKey`(newkey varchar(40))
+BEGIN
+   DELETE FROM `APIKEYS` WHERE keytext = newkey;
+END $$
+/*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
+
+DELIMITER ;
+
+--
+-- Definition of procedure `GetByKey`
+--
+
+DROP PROCEDURE IF EXISTS `GetByKey`;
+
+DELIMITER $$
+
+/*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetByKey`(newkey varchar(40))
+BEGIN
+   SELECT * FROM `APIKEYS` WHERE keytext = newkey;
+END $$
+/*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
+
+DELIMITER ;
+
+--
+-- Definition of procedure `GetByUser`
+--
+
+DROP PROCEDURE IF EXISTS `GetByUser`;
+
+DELIMITER $$
+
+/*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetByUser`(newEmail varchar(40))
+BEGIN
+   SELECT * FROM `APIKEYS` WHERE email = newEmail;
+END $$
+/*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
+
+DELIMITER ;
+
+--
+-- Definition of procedure `InsertKey`
+--
+
+DROP PROCEDURE IF EXISTS `InsertKey`;
+
+DELIMITER $$
+
+/*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertKey`(
+       newemail VARCHAR(255),
+       newkey VARCHAR(45),
+       newusage VARCHAR(1000),
+       newstate INTEGER(10))
+BEGIN
+
+      INSERT INTO `apikeys`(Email,
+      KeyText,UsageText,State)
+      values(newemail,newkey,newusage,newstate);
+END $$
+/*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
+
+DELIMITER ;
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
