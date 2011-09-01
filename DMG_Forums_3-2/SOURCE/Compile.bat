d3517fbeb1ad5d0c858a5cc3685dfadbed7bda76
Austin Montoya <Austin.Montoya.ctr@adlnet.gov>
2011-09-01 10:09:33 +0000

    Removing DMG_Forums_3-2 because it's not in our current plans. Will retrieve it from the bowels of Subversion later if necessary.

--- DMG_Forums_3-2/SOURCE/Compile.bat
+++
@@ -1,8 +0,0 @@
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\vbc /nologo /t:library /r:System.dll,System.web.dll,System.Data.dll,System.Web.ApplicationServices.dll,Microsoft.VisualBasic.dll,../bin/OrbitOne.OpenId.Controls.dll,../bin/OrbitOne.OpenId.MembershipProvider.dll,../bin/Simple.Providers.MySQL.dll /out:..\bin\Global.dll Global.vb
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\vbc /nologo /t:library /r:System.dll,System.web.dll,System.Data.dll,Microsoft.VisualBasic.dll,System.Xml.dll,../bin/Global.dll /out:..\bin\DMG.dll DMG.vb
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\vbc /nologo /t:library /r:System.dll,System.web.dll,System.Data.dll,Microsoft.VisualBasic.dll,../bin/Global.dll /out:..\bin\Forums.dll Forums.vb
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\vbc /nologo /t:library /r:System.dll,System.web.dll,System.Data.dll,Microsoft.VisualBasic.dll,System.Xml.dll,../bin/Global.dll /out:..\bin\Topics.dll Topics.vb
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\vbc /nologo /t:library /r:System.dll,System.web.dll,System.Data.dll,Microsoft.VisualBasic.dll,System.Xml.dll,../bin/Global.dll /out:..\bin\Members.dll Members.vb
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\vbc /nologo /t:library /r:System.dll,System.web.dll,System.Data.dll,Microsoft.VisualBasic.dll,System.Drawing.dll,../bin/Global.dll /out:..\bin\Upload.dll Upload.vb
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\vbc /nologo /t:library /r:System.dll,System.web.dll,System.Data.dll,../bin/Global.dll /out:..\bin\Admin.dll Admin.vb
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\vbc /nologo /t:library /r:System.dll,System.web.dll,System.Data.dll,Microsoft.VisualBasic.dll,../bin/Global.dll /out:..\bin\Setup.dll Setup.vb
