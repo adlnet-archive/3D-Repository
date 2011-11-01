9e0c8131a64bce00d4628c46b86b639906aeb3d7
Rob Chadwick <robert.chadwick.ctr@adlnet.gov>
2011-11-01 08:47:09 +0000

    fix for Remove GetAllContentObjects

--- VwarDal/IDataRepository.cs
+++ VwarDal/IDataRepository.cs
@@ -24,8 +24,8 @@ namespace vwarDAL
 {
     public interface IDataRepository
     {
-        IEnumerable<ContentObject> GetAllContentObjects();
-        IEnumerable<ContentObject> GetAllContentObjects(String UserName);
+        //IEnumerable<ContentObject> GetAllContentObjects();
+        //IEnumerable<ContentObject> GetAllContentObjects(String UserName);
         void InsertReview(int rating, string text, string submitterEmail, string contentObjectId);
         void UpdateContentObject(ContentObject co);
         
