f14059e294d89bf27914d158fc9f234551e6b05e
Rob Chadwick <robert.chadwick.ctr@adlnet.gov>
2012-01-31 16:13:48 -0500

    RebuildThumbnail cache tool not async, and a bit prettier

---
+++ VwarWeb/App_Code/RebuildThumbnailCache.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web.Security;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.IO;
+using System.Diagnostics;
+using vwarDAL;
+using System.Collections.Generic;
+using System.Web.Script.Services;
+using System.Web.Services;
+/// <summary>
+/// Summary description for RebuildThumbnailCache
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[ScriptService]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class RebuildThumbnailCache : System.Web.Services.WebService {
+
+    public RebuildThumbnailCache () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+    static IEnumerable<vwarDAL.ContentObject> allpids = null;
+    static vwarDAL.IDataRepository dal = null;
+    [WebMethod(EnableSession = true)]
+    [ScriptMethod]
+    public ArrayList GetAllPIDS()
+    {
+        dal = (new vwarDAL.DataAccessFactory()).CreateDataRepositorProxy();
+        allpids = dal.GetAllContentObjects();
+        ArrayList list = new ArrayList();
+        foreach (vwarDAL.ContentObject co in allpids)
+        {
+            list.Add(co.PID);
+        }
+        return list;
+    }
+    [WebMethod(EnableSession = true)]
+    [ScriptMethod]
+    public string UpdateThumbnailCache(string pid)
+    {
+      
+        foreach (vwarDAL.ContentObject co in allpids)
+        {
+            if (co.PID == pid)
+                try
+                {
+                    System.IO.Stream screenshotdata = dal.GetContentFile(co.PID, co.ScreenShot);
+                    if (screenshotdata != null)
+                    {
+                        int length = (int)screenshotdata.Length;
+
+                        if (length != 0)
+                        {
+
+                            string ext = new FileInfo(co.ScreenShot).Extension.ToLower();
+                            System.Drawing.Imaging.ImageFormat format;
+                            format = System.Drawing.Imaging.ImageFormat.Png;
+                            if (ext == ".png")
+                                format = System.Drawing.Imaging.ImageFormat.Png;
+                            else if (ext == ".jpg")
+                                format = System.Drawing.Imaging.ImageFormat.Jpeg;
+                            else if (ext == ".gif")
+                                format = System.Drawing.Imaging.ImageFormat.Gif;
+
+
+
+                            //Use the original file bytes to remain consistent with the new file upload ID creation for thumbnails
+                            co.ThumbnailId = Website.Common.GetFileSHA1AndSalt(screenshotdata) + ext;
+                            dal.UpdateContentObject(co);
+
+                            try
+                            {
+                                File.Delete(HttpContext.Current.Server.MapPath("~/thumbnails/" + co.ThumbnailId));
+                            }
+                            catch (System.IO.FileNotFoundException t)
+                            {
+
+                            }
+
+                            using (FileStream outFile = new FileStream(HttpContext.Current.Server.MapPath("~/thumbnails/" + co.ThumbnailId), FileMode.Create))
+                                Website.Common.GenerateThumbnail(screenshotdata, outFile, format);
+                        }
+                        else
+                        {
+                            return "No screenshot data";
+                        }
+                    }
+                    else
+                    {
+                        return "No screenshot data";
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    return ex.Message;
+                }
+        }
+        return "OK";
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+    
+    
+}
---
+++ VwarWeb/Controls/RebuildThumbnailCache.ascx
@@ -0,0 +1,65 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="RebuildThumbnailCache.ascx.cs" Inherits="Controls_RebuildThumbnailCache" %>
+<script type="text/javascript" src="../Scripts/jquery-ui-1.8.7.custom.min.js"></script>
+<div id="BeginRebuild">Rebuild Thumbnails</div>
+<div>
+        <table>
+            <tbody id="Log">
+                <tr>
+                    <td>PID</td>
+                    <td>Status</td>
+                </tr>
+            </tbody>
+        </table>
+</div>
+<script type="text/javascript">
+
+    $(document).ready(function () {
+        CreateWidgets();
+    });
+    var ServiceURL = '<%= Page.ResolveClientUrl("~/Users/RebuildThumbnailCache.asmx") %>';
+    function CreateWidgets() {
+
+        $("#BeginRebuild").button();
+        $("#BeginRebuild").click(function () {
+
+            $.ajax({
+                url: ServiceURL + "/GetAllPIDS",
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                data: null,
+                async: false,
+                success: function (res, status, jqXHR) {
+                    //alert(JSON.stringify(res));
+                    var pids = res.d;
+                    for (var i = 0; i < pids.length; i++) {
+
+                        var newentry = "<tr><td>" + pids[i] + "</td><td id='" + pids[i] + "'>" + "_" + "</td></tr>";
+                        $("#Log").html($("#Log").html() + newentry);
+                    }
+                    for (var i = 0; i < pids.length; i++) {
+
+                        $.ajax({
+                            url: ServiceURL + "/UpdateThumbnailCache",
+                            type: "POST",
+                            async: false,
+                            contentType: "application/json; charset=utf-8",
+                            dataType: "json",
+                            data: JSON.stringify({ "pid": pids[i] }),
+                            success: function (res, status, jqXHR) {
+                                // alert(res.d);
+                                document.getElementById(pids[i]).innerHTML = res.d;
+                            }
+                        });
+
+
+
+                    }
+                }
+            });
+
+        });
+    }
+
+
+</script>
